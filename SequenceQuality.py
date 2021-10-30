be9e38e110824b82abebc99faf4d1ab4f4ad002f
manojmw <74168582+manojmw@users.noreply.github.com>
2021-10-31 00:12:49 +0200

    Add files via upload

---
+++ SequenceQuality.py
@@ -0,0 +1,38 @@
+###Reading and Parsing sequence and Quality scores from a FASTQ file###
+
+###Reference: Python for Genomic Data Science Course, part of Specialization in Genomic Data Science by Johns Hopkins University on [Coursera]###
+
+#!/usr/bin/python
+
+print("This is a program to read and parse sequence and quality scores from a FASTQ file\n")
+try:
+    file = input("Please enter the name of the FASTQ file:")
+except IOError:
+    print("Cannot open the file!!!!....###Please try again###")
+
+def READFASTQ(file):
+    SEQ = ''
+    QUAL = ''
+    with open(file, 'r') as FHANDLE:
+        while True:
+            A = ''
+            B = ''
+            FHANDLE.readline()
+            A = FHANDLE.readline().rstrip()
+            FHANDLE.readline()
+            B = FHANDLE.readline().rstrip()
+            if len(A) == 0:
+                break
+            SEQ += A
+            QUAL += B
+    FHANDLE.close()
+    return SEQ, QUAL
+
+S, Q = READFASTQ(file)
+
+print("Here is your Sequence:\n")
+print(S)
+print("\n")
+
+print("Here is your Quality Score:\n")
+print(Q)
